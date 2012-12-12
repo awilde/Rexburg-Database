1c123a3282eb1c55c35d522a36a770e835815259
legoracer18 <legoracer18@gmail.com>
2012-12-12 09:37:16 -0800

    Create single_upload.sql

---
+++ single_upload.sql
@@ -0,0 +1,20 @@
+
+
+INSERT INTO inv_item
+( install_date  			
+, part_location				
+, part_type					
+, description				
+, created_by                
+, creation_date             
+, last_updated_by           
+, last_update_date )         
+VALUES
+( '1883-01-01'
+, 'Rexburg, Idaho'
+, '0'
+, 'City of Rexburg Technology Assets'
+, 1
+, UTC_DATE()
+, 1
+, UTC_DATE());
