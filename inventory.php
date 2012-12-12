8a0d4776446b434ea71ec0b34e2dd45380acada4
legoracer18 <legoracer18@gmail.com>
2012-12-12 09:33:18 -0800

    Create inventory.php

---
+++ inventory.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+  <head>  
+    <link href="/cit380/screen.css" rel="stylesheet" type="text/css" />
+    <title>Log-in</title>
+  </head>
+  <body>
+    <div id="header">
+      <div id="header_login">
+        <a href="http://1211.rexburg.org/pages/?action=login" title="Click to Logout">Log-In</a>
+        <p>Hello Guest</p>
+        <br><br>
+        <img src="http://1211.rexburg.org/images/tcs.png" id="header_login_image" alt="Header Login Image"/>
+        <div class="header_login_img">
+            <!--<img src="/images/city-logo-signature.png" alt=""/>-->		
+        </div>
+        <!--	-->
+      </div>
+
+      <div id="navigation">
+        <ol id="nav_ol">
+          <li><a href="http://1211.rexburg.org/pages/?action=openobjectives" title="View Open Objective Page">Objective</a></li>
+          <li><a href="http://1211.rexburg.org/pages/?action=timelog" title="View Time Log Page">Time Log</a></li>
+          <li><a href="http://1211.rexburg.org/pages/?action=timelog" title="View Inventory Log Page">Inventory</a></li>
+        </ol>
+      </div>		
+    </div>
+
+    <div id="main_content">
+      <div id="display_content">
+        <nav class="where_at">					
+          <!--<a href="/pages/admin/admin.php" title="">Admin Page</a> 					
+          <a href="/pages/admin/login.php" title=""><img src="/images/nav.jpg" alt="Nav space image" /> Login Page</a>-->
+        </nav>
+      </div>
+<!--**************************INSERT HERE****************************************-->
+      <div id="insert_content">
+        So this is the space that we have to add stuff to.  Do you just want a white background?
+      </div>
+<!--***********************END INSERT*****************************************-->
+    </div>
+
+    <div id="footer">
+      <div class="footer_image">
+        <div class="connect">
+          <img src="http://1211.rexburg.org/images/connect.png" alt="Footer Image"/>		
+        </div>
+
+        <div class="city_logo">
+          <img src="http://1211.rexburg.org/images/bw-city-logo-signature.png" alt="Footer Image"/>
+        </div>	
+      </div>
+      <span style="font-size:10pt;">0910.2012</span>		</div>
+  </body>
+</html>
